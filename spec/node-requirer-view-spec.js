a2349302be4ae28ab41674389ec5b333221477b8
Thomas Rich <trich1230@gmail.com>
2016-05-06 08:43:10 -0700

    messing around trying to make an atom package

---
+++ spec/node-requirer-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import NodeRequirerView from '../lib/node-requirer-view';
+
+describe('NodeRequirerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
